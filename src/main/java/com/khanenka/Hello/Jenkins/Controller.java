314809d01fa6c260eb9dbcd9e4fbe69b3a50ea8a
Khanenka <pozitivlen9@mail.ru>
2024-06-19 22:41:46 +0300

    first

---
+++ src/main/java/com/khanenka/Hello/Jenkins/Controller.java
@@ -0,0 +1,13 @@
+package com.khanenka.Hello.Jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @GetMapping
+  public String jenkins(){
+    return "Hello";
+  }
+}
